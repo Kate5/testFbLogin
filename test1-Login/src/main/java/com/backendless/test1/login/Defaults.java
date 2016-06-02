ed394bd538d205b6892f958198fb386cb4f634be
kate <kateryna.maksimenko@gmail.com>
2016-06-02 17:44:34 +0300

    Initial commit

---
+++ test1-Login/src/main/java/com/backendless/test1/login/Defaults.java
@@ -0,0 +1,9 @@
+package com.backendless.test1.login;
+
+public class Defaults
+{
+  public static final String APPLICATION_ID = "010125F0-FABC-A17A-FF7D-345B8C9B0F00";
+  public static final String SECRET_KEY = "04449459-2D8B-37D1-FF85-BFBFE2A0E000";
+  public static final String VERSION = "v1";
+  public static final String SERVER_URL = "http://api.backendless.com";
+}
